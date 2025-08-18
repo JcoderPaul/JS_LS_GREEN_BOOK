9b6d8f904a385661859797af9cfdfa9aaffb0a94
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-18 08:29:30 +0500

    JS_LS_GREEN_BOOK: add 4_js_code.js

---
+++ Js_ls_1_var/Examples/4_js_code.js
@@ -0,0 +1,8 @@
+// Динамическая типизация
+str = "foo" // String in Java
+
+console.dir(str)
+
+str = 3 // Как вариант int в Java и подобное присваивание не бросит ошибки в JS
+
+console.dir(str) // Запускаем проверяем
