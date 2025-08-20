7d8b414664b98aab98c25d78eb22217d8684d35d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-20 08:15:24 +0500

    JS_LS_GREEN_BOOK: add 18_js_functions_with_unmutable_object.js

---
+++ Js_ls_3_functions/Examples/18_js_functions_with_unmutable_object.js
@@ -0,0 +1,21 @@
+/* 
+Рекоммендуется не "мутировать" - не изменять внешние объекты внутри функций. 
+Пример с простым объектом, без вложенных объектов 
+*/
+
+const userOne = {
+        userName: "Paul",
+        userAge: 449
+}
+
+function divideAge(user) {
+        const newUser = {...user}       // Делаем копию "прилетевшего" в функцию объекта
+        newUser.userAge /= 2    // Ох и синтаксис, более понятно будет: newUser.userAge = newUser.userAge / 2  
+        return newUser
+}
+
+const userFromFun = divideAge(userOne)
+
+console.log(userOne)    // Исходный объект остался неизменным
+console.log("-------------------------------------------------")
+console.log(userFromFun)        // Получаем объект из функции см. рез.
