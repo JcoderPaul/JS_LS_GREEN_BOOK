3df385b1c8d47e140fbdd384a0447c4bc820ab66
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-22 06:18:10 +0500

    JS_LS_GREEN_BOOK: add 27_js_operators_typeOf.js

---
+++ Js_ls_5_operators/Examples/27_js_operators_typeOf.js
@@ -0,0 +1,18 @@
+/* TypeOf */
+console.log(typeof 10)       // 'number'
+
+console.log(typeof "Paul")       // 'string'
+
+console.log(typeof false)       // 'boolean'
+
+console.log(typeof undefined)       // 'undefined'
+
+/* Простые варианты применения */
+console.log("------------------------------------------------------------------")
+console.log(typeof 10 === 'number')       // 'true' 
+
+if(typeof 10 === 'number') {
+        console.log("true")
+}
+
+
