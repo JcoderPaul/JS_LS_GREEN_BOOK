efaddcd10b23232b22c14f9c2928f554695400b3
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-26 07:42:02 +0500

    JS_LS_GREEN_BOOK: update 41_5_js_array_to_string_and_back.js

--- Js_ls_9_arrays/Examples/41_5_js_array_to_string_and_back.js
+++ Js_ls_9_arrays/Examples/41_5_js_array_to_string_and_back.js
@@ -2,10 +2,10 @@ import brLine from "../../Js_ls_tasks/TasksAndSolutions/js_border_line.js";
 
 const url = 'auth/user/login';
 const resOfSplit = url.split('/'); // Задаем разделитель
-console.log(resOfSplit);
-console.log('Array length: ' + resOfSplit.length);
+console.log(resOfSplit); // [ 'auth', 'user', 'login' ]
+console.log('Array length: ' + resOfSplit.length); // Array length: 3
 brLine();
 
 const hero = ['Batman', 'Superman', 'Catwoman', 'PoisonIvy'];
 const strHeroChain = hero.join('-');
-console.log(strHeroChain);
+console.log(strHeroChain); // Batman-Superman-Catwoman-PoisonIvy
