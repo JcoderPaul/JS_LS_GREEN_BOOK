f33a100e42959534900e2ee2404853030c855ca0
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-27 07:48:34 +0500

    JS_LS_GREEN_BOOK: update 42_js_forEach.js

--- Js_ls_9_arrays/Examples/42_js_forEach.js
+++ Js_ls_9_arrays/Examples/42_js_forEach.js
@@ -2,11 +2,11 @@ const myNumberArray = new Array(1, 3, 5);
 console.log(myNumberArray); // [ 1, 3, 5 ]
 console.log("-----------------------------------------------------------------")
 
-/* Используем foreach с callback функцией увеличивающей каждый элемени в n */
+/* Используем foreach с callback функцией увеличивающей каждый элемент в n */
 const factor = 3; // Задаем множитель
 let myNewArray = new Array(); // Новый пустой массив
 
-/* Перебираем исходный массив, уножаем на factor и помещаем в новый массив */
+/* Перебираем исходный массив, умножаем на factor и помещаем в новый массив */
 let res = myNumberArray.forEach(element => myNewArray.push(element * factor));
 
 console.log("ForEach work resault: " + res); // undefined - forEach ничего не возвращает,
