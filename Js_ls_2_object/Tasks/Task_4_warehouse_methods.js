886edcc2d1e90053fbb6b9f2a9bf90711b4b5993
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-27 20:44:58 +0500

    JS_LS_GREEN_BOOK: add Task_4_warehouse_methods.js

---
+++ Js_ls_2_object/Tasks/Task_4_warehouse_methods.js
@@ -0,0 +1,68 @@
+/**
+ * Сделать объект склад. Сделать объекту методы:
+ * - добавления на склад;
+ * - поиск по складу товара;
+ * - рассчет веса всех товаров; 
+ */
+
+const warehouse = {
+        goods: [],
+        findGoodById: function (id){
+                const existedGood = this.goods.find(gFind => gFind.id === id);
+                return existedGood;
+        },
+        addGood: function (good){
+                const existedGood = this.findGoodById(good.id);
+                if (existedGood){
+                        console.log("Товар уже на складе!");
+                        return;
+                }
+                this.goods.push(good);
+        },
+        getWeightKg: function(){
+                return this.goods.reduce((acc, el) => {
+                        /* 
+                         Проверяем наличие поля вес через '.?', если поле есть суммируем с 
+                         'аккумуляторм', если нет, как в 'paper', тогда с 'аккумуляторм' 
+                         суммируется ноль.  
+                        */
+                        return acc += el.weight?.kg ? el.weight.kg : 0;
+                }, 0);
+        }
+};
+
+const car = {
+        id: 1,
+        weight: {kg: 1000},
+        brand: 'Lada'
+};
+
+const chair = {
+        id: 2,
+        weight: {kg: 2},
+
+}
+
+const paper = {
+        id: 3,
+        color: 'red'
+}
+
+/* 'Тест' */
+
+warehouse.addGood(car); 
+console.log(warehouse.goods); // [ { id: 1, weight: { kg: 1000 }, brand: 'Lada' } ]
+
+warehouse.addGood(car); // Товар уже на складе!
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+console.log(warehouse.goods); // Получаем массив 3-х добавленных объектов
+
+let findItem = warehouse.findGoodById(6); // Такого id у нас нет
+console.log(findItem); // undefined
+
+findItem = warehouse.findGoodById(1); // Такого id у нас нет
+console.log(findItem); // { id: 1, weight: { kg: 1000 }, brand: 'Lada' }
+
+const allWeight = warehouse.getWeightKg();
+console.log(allWeight); // 1002
