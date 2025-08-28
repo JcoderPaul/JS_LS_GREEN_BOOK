5e8f9b36e64438c091b7bab52e272bb5d1507835
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-28 05:42:48 +0500

    JS_LS_GREEN_BOOK: add 45_js_array_distructing.js

---
+++ Js_ls_11_destructuring/Examples/45_js_array_distructing.js
@@ -0,0 +1,6 @@
+const fruits = ['Apple', 'Banana', 'Orange'];
+
+const [fruitOne, fruitTwo] = fruits; // Согласно порядку следования содержимого массива
+
+console.log(fruitOne); // Apple
+console.log(fruitTwo); // Banana
