14739cb660d7ab96a15d81557899d96f6069b57b
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-29 07:39:44 +0500

    JS_LS_GREEN_BOOK: add 47_2_true_switch.js

---
+++ Js_ls_12_conditional_instructions/Examples/47_2_true_switch.js
@@ -0,0 +1,12 @@
+const num = -1;
+
+switch(true){
+        case num > 0:
+                console.log("Положительный!");
+                break;
+        case num < 0:
+                console.log("Отрицательный!");
+                break;
+        default:
+                console.log("Определенно ноль!");           
+}
