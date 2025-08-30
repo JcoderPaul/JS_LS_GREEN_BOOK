7e46264ebc84cf41ef5d7bfa6d643fd366ab5a79
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-30 06:08:10 +0500

    JS_LS_GREEN_BOOK: add js_str_split_and_join_7.js

---
+++ Js_ls_13_string/Example/js_str_split_and_join_7.js
@@ -0,0 +1,14 @@
+const userNameWithNick = 'Duglas aka deep digging Lind';
+
+const arrFromStr = userNameWithNick.split(' ');
+console.log(arrFromStr);
+
+/* Деструктуризация */
+const [firstName, , , , surname] = userNameWithNick.split(' ');
+console.log(firstName);
+console.log(surname);
+
+/* Объеденим массив в строку */
+const strToPrn = arrFromStr.join(' ');
+console.log(strToPrn);
+
