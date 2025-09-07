a2a1ba3ac5018a5d944da1450e6f1f61e13a04b7
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-09-07 07:38:39 +0500

    JS_LS_GREEN_BOOK: add 70_js_classes_styles_manipulation.js

---
+++ Js_ls_21_DOM_document_object_model/Examples/70_DOM_js_classes_styles_manipulation/70_js_classes_styles_manipulation.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/* 
+Попробуем взаимодействовать с таблицей стилей CSS и добавим динамики в страницу.
+Добавим поле с текстом, оно будет менять видимость в зависимости от наших действий.
+*/
+
+/* 'Универсальный' обработчик действий */
+function submitForm(){
+        const input = document.querySelector('.input').value;
+        if (!input){
+                return;
+        }
+        document.querySelector('.panel').innerText = input;
+        document.querySelector('.input').value = '';
+        
+        /* 
+        В таком варианте, в процессе работы, изменения как бы ложаться сверху не меняя свойства самого класса 
+        'notification', это будет работать: 
+
+        document.querySelector('.notification').style.display = 'block';
+
+        см. ..\Js_ls_21_DOM_document_object_model\DOC\CSS_HTML_interaction_interfaces\elementStyleProperty.md
+
+        Но, что если нужно сделать такие замены во многих местах и менять не только свойства отображения, а 
+        например шрифт, цвет и т.п.
+
+        Лучше сделать немного по другому. Добавим классу изменения свойств в файле CSS. И обратимся к самому 
+        классу. Для наглядности отобразим два варианта добавление и удаление классов (см. структуру строк в 
+        index.html и краткое описание свойства): 
+        ..\Js_ls_21_DOM_document_object_model\DOC\CSS_HTML_interaction_interfaces\classListProperty.md
+        */
+        document.querySelector('.notification').classList.add('notification_active'); // Не видна до изменений, видна после
+        document.querySelector('.recommendation').classList.remove('recommendation_display'); // Видна до изменений, не видна после
+
+}
+
+/* Обработчик кликов мышью - теоретически без него можно обойтись сразу используя submitForm() */
+function changeMouseClick(){
+        submitForm();
+}
+
+/* 
+Слушатель нажатия клавишь, чтобы он сработал дополним строку: 
+<input class="input" /> в index.html атрибутом 'onkeydown'  
+*/
+function changeKeyboardInput(event) {
+        if (event.code == 'Enter') {
+                submitForm();
+        }
+}
+
