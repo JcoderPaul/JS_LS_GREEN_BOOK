a7c4418279a96ad7043e10fa5484f672f4f18a4b
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-09-07 07:37:06 +0500

    JS_LS_GREEN_BOOK: add 68_js_fun_key_processing.js

---
+++ Js_ls_21_DOM_document_object_model/Examples/68_2_DOM_js_fun_key_processing/68_js_fun_key_processing.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* 
+Вынесем логику обработки событий, нажатие на кнопку фомы, в отдельную функцию,
+т.е. все, что мы делали в прошлом примере, мы оформляем в виде 'универсальной' 
+функции.
+
+Теперь чтобы все заработало, нужно внести небольшие изменения в index.html, мы 
+должны явно в строке: <button class="button">Change</button>, прописать 'повидение'
+т.е. вызвать нашу функцию: onclick="changeClick().
+*/
+
+function changeClick(){
+        const input = document.querySelector('.input').value;
+        if (!input){
+                return;
+        }
+        document.querySelector('.panel').innerText = input;
+        document.querySelector('.input').value = '';
+}
+
