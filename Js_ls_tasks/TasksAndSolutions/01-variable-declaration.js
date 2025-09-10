51319e41aaa53d572279fe89e055005c7dfb5945
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-09-10 07:45:50 +0500

    JS_LS_GREEN_BOOK: add ls_tasks 01-variable-declaration.js

---
+++ Js_ls_tasks/TasksAndSolutions/01-variable-declaration.js
@@ -0,0 +1,13 @@
+/** ЗАДАЧА 1 - Объявление переменной
+ *
+ * 1. Объявите набор переменных: "myCountry", "myName", "myAge" присвойте им значения.
+ * 2. Выведите значение переменной в консоль
+ */
+
+const myCountry = "Narniya";
+const myName = "me.OldBoy"
+let myAge = 48;
+
+console.log(myCountry);
+console.log(myName);
+console.log(myAge);
